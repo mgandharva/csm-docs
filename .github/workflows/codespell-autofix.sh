cb8341cf0c4f3655442c96891566b713ba38c42b
mgandharva <mukesh.gandharva@dell.com>
2025-01-27 07:02:58 -0500

    code updated

--- .github/workflows/codespell-autofix.sh
+++ .github/workflows/codespell-autofix.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-
+script_dir=$(dirname "$(readlink -f "$0")")
+echo "Script dir: $script_dir"
 # Run codespell and capture output
-output=$(find . -type f -name "*.md" -exec codespell --ignore-words=.github/workflows/codespell.txt --builtin clear,rare,informal,code,usage --write-changes --only-per-line --quiet-level=2 {} +)
+output=$(find ../.. -type f -name "*.md" -exec codespell --ignore-words=."$script_dir"/codespell.txt --builtin clear,rare,informal --write-changes --quiet-level=0 {} +)
 
-echo "$output"
+echo "Output: $output"
 
 # Process the output and apply the first suggestion
 echo "$output" | while read -r line; do
     if [[ $line == *"=="* ]]; then
         file=$(echo "$line" | cut -d':' -f1)
         error_line=$(echo "$line" | cut -d':' -f2)
-        original=$(echo "$line" | cut -d':' -f3 | cut -d' ' -f2)
+        original=$(echo "$line" | awk -F ' ' '{print $2}')
         suggestion=$(echo "$line" | grep -oP '(?<= ==>)\s\K[^,]*')
-
+        
         # Apply the first suggestion
         if [ -n "$suggestion" ] && [ -f "$file" ]; then
             sed -i "${error_line}s/$original/$suggestion/" "$file"
