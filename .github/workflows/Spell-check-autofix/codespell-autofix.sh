a5d16fa530d8e9d4ca76de2c381c7e4380707fcf
mgandharva <mukesh.gandharva@dell.com>
2025-01-28 01:35:36 -0500

    code updated

--- .github/workflows/Spell-check-autofix/codespell-autofix.sh
+++ .github/workflows/Spell-check-autofix/codespell-autofix.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
+
+# Determine the directory of the script
 script_dir=$(dirname "$(readlink -f "$0")")
 echo "Script dir: $script_dir"
-# Run codespell and capture output
-output=$(find ../.. -type f -name "*.md" -exec codespell --ignore-words="$script_dir"/codespell.txt --builtin clear,rare,informal --write-changes --quiet-level=0 {} +)
+
+# Define the target directory
+target_dir=$(realpath "$script_dir/../../..")
+
+echo "Target dir: $target_dir"
+# Run codespell and capture output, targeting the test-csm-docs directory
+output=$(find "$target_dir" -type f -name "*.md" -exec codespell --ignore-words="$script_dir/codespell.txt" --builtin clear,rare,informal --write-changes --quiet-level=0 {} +)
 
 echo "Output: $output"
 
 # Process the output and apply the first suggestion
-echo "$output" | while read -r line; do
+echo "$output" | while IFS= read -r line; do
     if [[ $line == *"=="* ]]; then
         file=$(echo "$line" | cut -d':' -f1)
         error_line=$(echo "$line" | cut -d':' -f2)
